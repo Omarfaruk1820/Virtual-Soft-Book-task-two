df68aa4c67e6a5377b3117fd5f71d647b69f4cbf
omarfaruk1820 <omarfarukfci9th@gmil.comm>
2024-05-18 21:34:35 +0600

    footer section added

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+
+const Footer = () => {
+    return (
+        <div className='mx-5'>
+            <footer className="footer p-10  bg-slate-900 mt-10 text-base-content ">
+  <aside>
+    <h1 className='text-5xl font-extrabold text-white'>evaly</h1>
+    <p className='text-white text-xl'>Largest product search engine, maximum<br></br> categorized online shopping mall<br></br> and quickest home delivery system.</p>
+  </aside> 
+  <nav className='text-white text-xl'>
+    <h6 className="footer-title">Contact Us</h6> 
+    <a className="link link-hover">House #8, Road # 14,</a>
+    <a className="link link-hover">Dhanmondi, Dhaka-1209.</a>
+    <a className="link link-hover">Email: support@e-valy.com</a>
+
+  </nav> 
+  <nav className='text-white text-xl'>
+    <h6 className="footer-title">Let Us Help You</h6> 
+    <a className="link link-hover">Your Account</a>
+    <a className="link link-hover">Your Order</a>
+    <a className="link link-hover">Terms & Conditions</a>
+    <a className="link link-hover">FAQ</a>
+  </nav> 
+  <nav className='text-white text-xl'>
+    <h6 className="footer-title">Get Evaly App</h6> 
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+</footer>
+<footer className="footer items-center  p-4 bg-neutral text-neutral-content">
+  <aside className="items-center  grid-flow-col">
+    <h1 className='text-3xl font-extrabold ml-20'>evaly</h1>
+    
+    <p className='text-xl text-white ml-20'>2024 E-valy.com Limited. All rights reserved.</p>
+    
+    </aside> 
+  
+</footer>
+        </div>
+    );
+};
+
+export default Footer;
--- src/LayOut/Main.jsx
+++ src/LayOut/Main.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "../Components/Header/Header";
 import NavigationBar from "../Components/Navigation/NavigationBar";
 import Banner from "../Components/Banner/Banner";
+import Footer from "../Components/Footer/Footer";
 
 
 
@@ -13,6 +14,7 @@ const Main = () => {
         <div className='max-w-[1400px] mx-auto mt-5'>
         <Banner></Banner>
         </div>
+        <Footer></Footer>
             <Outlet></Outlet>
         </div>
     );
